58687a9c6560f3d43abaa29638fe88db3b9ee990
fulgence nahimana <funfulgence@gmail.com>
2023-04-07 03:16:15 -0700

    updates

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* this function will print the binary representation of a number
+* @n: number to print in binary
+* main - check the code
+* Return: no return
+* whritten by  Fulgence nahimana
+*/
+void print_binary(unsigned long int n)
+{
+if (n >> 0)
+{
+if (n >> 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
+}
+else
+{
+_putchar('0');
+}
+}
